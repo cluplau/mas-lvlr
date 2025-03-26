9105d65e3ee978e925f33eeb0ad12e9e8c7754d1
luppilau <cl@estaid.dk>
2025-03-26 16:56:13 +0100

    meta tags fix

---
+++ src/lib/MetaTags.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { MetaTags } from 'svelte-meta-tags';
+</script>
+
+<MetaTags
+	title="Level maker"
+	titleTemplate="%s | 02285 Artificial Intelligence and Multi-Agent Systems"
+	description="Create, modify, and visualize levels for the hospital domain in the DTU course: 02285 Artificial Intelligence and Multi-Agent Systems."
+	canonical="https://mas-lvlr.vercel.app"
+	openGraph={{
+		url: 'https://mas-lvlr.vercel.app',
+		title: 'Level maker | 02285 Artificial Intelligence and Multi-Agent Systems',
+		description:
+			'Create, modify, and visualize levels for the hospital domain in the DTU course: 02285 Artificial Intelligence and Multi-Agent Systems.',
+		images: [
+			{
+				url: 'https://mas-lvlr.vercel.app/meta-image.png',
+				alt: 'Meta Image'
+			}
+		],
+		siteName: '02285 Artificial Intelligence and Multi-Agent Systems',
+		type: 'website'
+	}}
+	twitter={{
+		creator: '@luppilau',
+		cardType: 'summary_large_image',
+		title: 'Level maker | 02285 Artificial Intelligence and Multi-Agent Systems',
+		description:
+			'Create, modify, and visualize levels for the hospital domain in the DTU course: 02285 Artificial Intelligence and Multi-Agent Systems.',
+		image: 'https://mas-lvlr.vercel.app/meta-image.png',
+		imageAlt: 'Meta Image'
+	}}
+/>
